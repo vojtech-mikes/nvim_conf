a76b3adf24838d9031ae5c1cd08685734fabc699
Vojtech Mikes <vojtech.mikes@epptec.eu>
2023-12-18 10:41:29 +0100

    Added binding for open compeltion on NVIM CMP

--- init.lua
+++ init.lua
@@ -99,6 +99,7 @@ local cmp = require('cmp')
 cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		['<CR>'] = cmp.mapping.confirm({select = true}),
+		['<C-Space>'] = cmp.mapping.complete(),
 	}),
 	preselect = 'item',
 	completion = {
